f7448b6bec1f5463d7fe5ec5ded8c30b210080b8
sejan2 <sejansiju2020@gmail.com>
2021-10-31 19:51:13 +0600

    second

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -46,6 +47,23 @@ async function run() {
             const myorder = await cursor.toArray();
             res.send(myorder)
         })
+
+        app.delete('/myOrder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            console.log('delete user', result)
+            res.json(result)
+        })
+
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serveCollection.insertOne(service)
+            console.log('yahoo', result)
+            res.json(result)
+        })
+
+
     }
     finally {
         // await client .close()
